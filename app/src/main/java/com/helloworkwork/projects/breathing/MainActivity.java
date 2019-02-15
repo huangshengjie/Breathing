1ea0e5563b2cac5f53ae1ad457fe832f7b39214f
huangshengjie <helloworkwork@outlook.com>
2019-02-15 22:38:04 +0800

    Initial commit

---
+++ app/src/main/java/com/helloworkwork/projects/breathing/MainActivity.java
@@ -0,0 +1,31 @@
+package com.helloworkwork.projects.breathing;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
